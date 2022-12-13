77440531a9a5a1c9780982177e30b13afa7e646a
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-14 04:54:57 +0500

    Create Raad cruise missile.py

---
+++ Raad cruise missile.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+from hashlib import blake2b
+blake2b(b'RAAD cruise missile').hexdigest()
+'2d52de50ca80b0d7c7de4d1731028dcbc52ee2043575f356723794cedc62f5fe10a48d7b52ab2931eccefcf7d6d69e93b369d6effe2254149b4770e5c9aabbc9'
